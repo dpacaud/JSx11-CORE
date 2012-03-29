cd606227a1864e9ff09dc878fcdd34761f43faec
Damien Pacaud <damien.pacaud@gmail.com>
2012-03-29 23:57:05 +0200

    secu

--- modules/auth.js
+++ modules/auth.js
@@ -3,7 +3,7 @@ var unixlib = require("unixlib");
 // Change accordingly or write your own.
 var service = "system-auth";
 var username = "dpacaud";
-var password = "dpacaud12345";
+var password = "";
 
 unixlib.pamauth(service, username, password, function(result) {
     console.log("Username: " + username + ", password: " + password + ", result: " + result);
